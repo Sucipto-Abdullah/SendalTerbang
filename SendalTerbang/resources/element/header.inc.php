f25b304b0e22f988bc7cc88693edee5eeb9f667a
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-09-26 10:51:15 +0700

    header Update

--- SendalTerbang/resources/element/header.inc.php
+++ SendalTerbang/resources/element/header.inc.php
@@ -6,8 +6,13 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><head>
+    <!-- Bootstrap CSS (opsional kalau belum ada) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
     <link rel="stylesheet" href="<?php url('CSS/general.css')?>">
     <style>
         :root{
@@ -22,47 +27,68 @@
             height: 58px;
             width: 100%;
             background-color: var(---white);
+            box-shadow: 2px 0 2px black;
         }
         .content{
             margin: auto;
             width: max(960px);
             height: 100%;
-            display: flex;
+            display: grid;
+            grid-template-columns: 15% 70% 15%;
+            grid-template-areas: "logo searchBar menu";
         }
 
         .logo{
             margin: auto;
+            margin-left: 0;
             height: 100%;
         }
         p{
             align-content: center;
             margin: auto;
             height: 100%;
+            font-size: 16px;
         }
 
         .searchBar{
             display: flex;
             height: 100%;
+            width: 500px;
             margin: auto;
             align-content: center;
         }
         #searchInput{
             margin: auto;
             align-content: center;
-            height: 20px;
+            height: 24px;
+            width: 300px;
+            font-size: 12px;
+            text-align: center;
+            border-style: solid;
+            border-width: 1px;
+            border-color: black;
+        }
+        #searchInpus::focus{
+            border-width: 2px;
+            border-color: pink;
         }
 
         .ButtonElement{
+            margin: auto;
+            margin-right: 0;
             height: 100%;
             align-content: center;
-            margin: auto;
+            width: 50px;
         }
         .buttonMenu{
+            margin: auto;
             background-color: rgba(0, 0, 0, 0);
-            width: 24px;
-            height: 24px;
+            text-align: right;
+            width: 100% ;
+            height: 100%;
             border: none;
-            color: blue;
+            font-size: 24px;
+            color: black;
         }
 
     </style>
@@ -73,19 +99,17 @@
         
         <div class="content">
 
-            <div class="logo">
-                <p>Sendal Terbang</p>
+            <div class="logo" style="grid-area: logo;">
+                <p><i class="bi bi-gear-fill"></i> Sendal Terbang</p>
             </div>
-            <div class="searchBar">
+            <div class="searchBar" style="grid-area: searchBar;">
                 <input id="searchInput" placeholder="Cari Proyek" >
             </div>
-            <div class="ButtonElement">
-                <button class="buttonMenu"> <img src="../resources/icon/list.svg"></button>
+            <div class="ButtonElement" style="grid-area: menu;">
+                <button class="buttonMenu"> <i class="bi bi-list"></i></button>
             </div>
 
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </html>
