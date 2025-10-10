9ffe38c2b8fb0520455eb5c3e30fd1e87af12a08
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-10-10 08:38:07 +0700

    expo page updated

--- SendalTerbang/resources/views/expo_page.php
+++ SendalTerbang/resources/views/expo_page.php
@@ -10,7 +10,7 @@
     
     <div class='project-card'>
 
-        <img src="images/chips.jpg" alt="Inituh gambar project" style="grid-area: image;">
+        <img class="project-image" src="images/chips.jpg" alt="Inituh gambar project" style="grid-area: image;">
 
         <h1 class="project-title" style="grid-area: title;" >Judul Projectnya</h1>
         <p class="project-desc" style="grid-area: desc;" >Deskripsi singkat projectnya</p>
